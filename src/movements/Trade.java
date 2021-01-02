ac496b77cb521eb34c016d08def3e5cfced89a66
Paul <paulhaeussler@gmx.de>
2021-01-02 21:41:54 +0100

    nokill

--- src/movements/Trade.java
+++ src/movements/Trade.java
@@ -1,15 +1,20 @@
 package movements;
 
 
+import com.google.gson.internal.LinkedTreeMap;
 import main.ApiController;
+import util.Printer;
 import util.Utility;
 
+import java.text.DecimalFormat;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 
+import static main.Test.api;
 import static movements.Option.STATE.BUYBACK;
 import static movements.Option.STATE.OPEN;
+import static util.Printer.LOGTYPE.INFO;
 
 /**
  * A trade is all option buys, sells and deliveries of one specific instrument combined
@@ -103,10 +108,10 @@ public class Trade extends Movement {
 
         timeRemaining = state == OPEN ? convertMillisToHMmSs(expiryDate.getTime() - System.currentTimeMillis()) : convertMillisToHMmSs(0);
         change = opChange;
-        //change = state == OPEN ? estimateValue(op, openPos, opChange, index, (expiryDate.getTime() - System.currentTimeMillis()), (expiryDate.getTime() - (long)timestamp)) : opChange;
+        change = state == OPEN ? estimateValue(op, openPos, opChange, index, (expiryDate.getTime() - System.currentTimeMillis()), (expiryDate.getTime() - (long)timestamp)) : opChange;
     }
 
-    private static Double estimateValue(Option op, double openPos, double change, double index, long timeRemaining, long initialRuntime){
+    private Double estimateValue(Option op, double openPos, double change, double index, long timeRemaining, long initialRuntime){
         double diff = 0.0;
 
         if(op.kind == Option.KIND.PUT && op.strikePrice > index){
@@ -119,6 +124,39 @@ public class Trade extends Movement {
         double effectiveDiff = liability + change * index;
         double btcEffDiff = effectiveDiff / index;
 
+        LinkedTreeMap map = null;
+        try{
+            this.index = api.getIndex(this.currency);
+            map = api.getBookSummary(this.instrumentName);
+        } catch (Exception e){
+            Printer.printException(e);
+            System.exit(1);
+        }
+
+        //pos diff == out of money; neg diff == in the money
+        this.diffToStrike = 0.0;
+
+        if(this.kind == Option.KIND.PUT){
+            this.diffToStrike = this.index - this.strikePrice;
+        }
+        if(this.kind == Option.KIND.CALL){
+            this.diffToStrike = this.strikePrice - this.index;
+        }
+
+        this.maxGain = 0.0;
+
+
+        this.ask = (Double) map.get("ask_price");
+        this.bid = (Double) map.get("bid_price");
+        this.avgPrem = Math.abs(this.maxGain / this.openPos);
+        this.priceDiff = (this.bid - this.avgPrem) * this.openPos;
+        this.valIfDelivery = this.diffToStrike * this.openPos;
+        this.currValue = Math.max(this.priceDiff, this.valIfDelivery);
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        DecimalFormat df2 = new DecimalFormat("0.00000");
+        Printer.printToLog(this.instrumentName + " diff to strike: " + df.format(this.diffToStrike) + "; PaidPrice: " + df2.format(this.change) + " - Bid: " + df2.format(this.bid) + "; CurrVal: " + df2.format(this.currValue), INFO);
+        
         double result = btcEffDiff;
 
         if(result > 0) {
